0006d6517a6c5e008d294d5be52fccc00efa762e
harlei akira <harlei.akira@gmail.com>
2023-09-07 22:32:17 -0300

    adicionando imagens

---
+++ E-Commerce-main/src/utilidades.js
@@ -0,0 +1,124 @@
+export const catalogo = [
+  {
+    id: "1",
+    marca: "KitKat",
+    nome: "Abacaxi",
+    preco: 70,
+    imagem: "abacaxi.png",
+    feminino: false,
+  },
+  {
+    id: "2",
+    marca: "KitKat",
+    nome: "Algodão Doce",
+    preco: 85,
+    imagem: "algodao-doce.png",
+    feminino: true,
+  },
+  {
+    id: "3",
+    marca: "KitKat",
+    nome: "Clássico ao leite",
+    preco: 60,
+    imagem: "ao-leite.png",
+    feminino: false,
+  },
+  {
+    id: "4",
+    marca: "KitKat",
+    nome: "Avelã",
+    preco: 160,
+    imagem: "avela.png",
+    feminino: false,
+  },
+  {
+    id: "5",
+    marca: "KitKat",
+    nome: "Banana",
+    preco: 110,
+    imagem: "banana.png",
+    feminino: false,
+  },
+  {
+    id: "6",
+    marca: "KitKat",
+    nome: "Café",
+    preco: 170,
+    imagem: "cafe.png",
+    feminino: true,
+  },
+  {
+    id: "7",
+    marca: "KitKat",
+    nome: "Churros",
+    preco: 75,
+    imagem: "churros.png",
+    feminino: true,
+  },
+  {
+    id: "8",
+    marca: "KitKat",
+    nome: "Coco",
+    preco: 88,
+    imagem: "coco.png",
+    feminino: true,
+  },
+];
+
+export function salvarLocalStorage(chave, informacao) {
+  localStorage.setItem(chave, JSON.stringify(informacao));
+}
+
+export function lerLocalStorage(chave) {
+  return JSON.parse(localStorage.getItem(chave));
+}
+
+export function apagarDoLocalStorage(chave) {
+  localStorage.removeItem(chave);
+}
+
+export function desenharProdutoCarrinhoSimples(
+  idProduto,
+  idContainerHtml,
+  quantidadeProduto
+) {
+  const produto = catalogo.find((p) => p.id === idProduto);
+  const containerProdutosCarrinho = document.getElementById(idContainerHtml);
+
+  const elementoArticle = document.createElement("article"); //<article></article>
+  const articleClasses = [
+    "flex",
+    "bg-stone-200",
+    "rounded-lg",
+    "p-1",
+    "relative",
+    "mb-2",
+    "w-96",
+  ];
+
+  for (const articleClass of articleClasses) {
+    elementoArticle.classList.add(articleClass);
+  }
+  //<article class="flex bg-slate-100 rounded-lg p-1 relative"></article>
+
+  const cartaoProdutoCarrinho = `
+    <img
+      src="./assets/img/${produto.imagem}"
+      alt="Carrinho: ${produto.nome}"
+      class="h-24 rounded-lg"
+    />
+    <div class="p-2 flex flex-col justify-between">
+      <p class="text-slate-900 text-sm">
+        ${produto.nome}
+      </p>
+      <p class="text-slate-400 text-xs">Tamanho: M</p>
+      <p class="text-green-700 text-lg">$${produto.preco}</p>
+    </div>
+    <div class='flex text-slate-950 items-end absolute bottom-0 right-2 text-lg'>
+        <p id='quantidade-${produto.id}' class='ml-2'>${quantidadeProduto}</p>
+    </div>`;
+  //<article class="flex bg-slate-100 rounded-lg p-1 relative">codigo do cartao do produto</article>
+
+  elementoArticle.innerHTML = cartaoProdutoCarrinho;
+  containerProdutosCarrinho.appendChild(elementoArticle);
+}
